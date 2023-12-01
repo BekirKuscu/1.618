718df130f27ba6bfb11f72f49038891d68b8a137
BEKIRKSU <bekirkscu@gmail.com>
2023-12-01 17:09:39 +0000

    Navbar updated
    Navbar updated to match the page with font and style. But more changes will be made.

--- src/Components/Navbar-Content/Navbar.js
+++ src/Components/Navbar-Content/Navbar.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./Navbar.css";
+import './Navbar.css'; 
 
-export default function Navbar() {
-  return (
-    <div>
-      <header role="banner" className="ui-section-header">
-        <div className="ui-layout-container">
-          <div className="ui-section-header__layout ui-layout-flex">
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">
+                        HOME
+                        <div className="underline"></div>
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/MoreInfo" className="nav-link">
+                        DEMO
+                        <div className="underline"></div>
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/DonationsPage" className="nav-link">
+                        DONATIONS
+                        <div className="underline"></div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
-            <Link to="/" role="link" aria-label="#" className="ui-section-header--logo">
-              <img width="60" src="https://cdn.dribbble.com/users/179241/screenshots/6116810/chris-fernandez-electric-recycling-editorial-1-dribbble.png" alt="Logo" />
-            </Link>
-            <input type="checkbox" id="ui-section-header--menu-id" />
-            <label htmlFor="ui-section-header--menu-id" className="ui-section-header--menu-icon"></label>
-
-            <nav role="navigation" className="ui-section-header--nav ui-layout-flex">
-              <Link to="/" role="link" aria-label="#" className="ui-section-header--nav-link">Home</Link>
-              <Link to="/MoreInfo" role="link" aria-label="#" className="ui-section-header--nav-link">MoreInfo</Link>
-              <Link to="/DonationsPage" className="ui-section-header--nav-link">Donations</Link>
-              <a href="https://github.com/BEKIRKSU/1.618" role="link" aria-label="#" className="ui-section-header--nav-link">GitHub</a>
-            </nav>
-          </div>
-        </div>
-      </header>
-    </div>
-  );
-}
+export default Navbar;
