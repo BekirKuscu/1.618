4b199ac73e7213e50e8d3f640e4fd2f5ffc3a5a9
onurat <114289826+onurat@users.noreply.github.com>
2023-12-07 17:21:30 +0000

    Added Welcome
    new section to homepage added.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Header from '../../Components/Header-Content/Header';
 import WasteDetail from '../../Components/WasteDetail-Content/WasteDetail';
-// import WasteQuote from '../../Components/WasteQuotes-Conten/WasteQuote';
 import TeamSection from '../../Components/Our-Team/TeamSection';
-
+import Welcome from '../../Components/Welcome-Content/Welcome';
 function Home() {
 return (
     <div>
+       <Welcome />
       <Header />
       <WasteDetail />
       <TeamSection />
-      {/* <WasteQuote /> */}
     </div>
 )
   }
