d3b3186bf76160f2f762fb0857ea1b7d4d7cba95
onurat <114289826+onurat@users.noreply.github.com>
2023-12-07 18:22:41 +0000

    new content
    Added new content to our homepage with info about us.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,11 +3,13 @@ import Header from '../../Components/Header-Content/Header';
 import WasteDetail from '../../Components/WasteDetail-Content/WasteDetail';
 import TeamSection from '../../Components/Our-Team/TeamSection';
 import Welcome from '../../Components/Welcome-Content/Welcome';
+import OurMission from '../../Components/OurMission-Content/OurMission';
 function Home() {
 return (
     <div>
-       <Welcome />
+      <Welcome />
       <Header />
+      <OurMission />
       <WasteDetail />
       <TeamSection />
     </div>
