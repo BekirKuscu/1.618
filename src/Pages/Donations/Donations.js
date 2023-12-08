a6b25eb78b525097b205f2916fd2594a5f045a31
onurat <114289826+onurat@users.noreply.github.com>
2023-12-08 19:50:04 +0000

    Update Donations.js
    image replaced with hd

--- src/Pages/Donations/Donations.js
+++ src/Pages/Donations/Donations.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import './DonationsPage.css';
 
@@ -31,55 +30,59 @@ const DonationsPage = () => {
 
   return (
     <div>
-       <img src="https://images.unsplash.com/photo-1655666582790-6ddaf5f194a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="Nature Image" class="nature-image"></img>
+      <img
+        src="https://images.unsplash.com/photo-1466611653911-95081537e5b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Nature Image"
+        className="nature-image"
+      />
       <h1>Donations</h1>
       <p className='donations-quote'>"Recycle for a greener tomorrow, donate to save lives today. Small actions, big impact."</p>
       <div className="donationsBox">
         <h2>Donate Now</h2>
         <div className="time-toggle">
-    <div className="time-option">
-        <input
-            type="radio"
-            id="weekly"
-            name="time"
-            value="weekly"
-            checked={selectedTime === 'weekly'}
-            onChange={handleTimeChange}
-        />
-        <label htmlFor="weekly">Weekly</label>
-    </div>
-    <div className="time-option">
-        <input
-            type="radio"
-            id="monthly"
-            name="time"
-            value="monthly"
-            checked={selectedTime === 'monthly'}
-            onChange={handleTimeChange}
-        />
-        <label htmlFor="monthly">Monthly</label>
-    </div>
-    <div className="time-option">
-        <input
-            type="radio"
-            id="monthly"
-            name="time"
-            value="monthly"
-            checked={selectedTime === 'monthly'}
-            onChange={handleTimeChange}
-        />
-        <label htmlFor="monthly">One-off</label>
-    </div>
-</div>
+          <div className="time-option">
+            <input
+              type="radio"
+              id="weekly"
+              name="time"
+              value="weekly"
+              checked={selectedTime === 'weekly'}
+              onChange={handleTimeChange}
+            />
+            <label htmlFor="weekly">Weekly</label>
+          </div>
+          <div className="time-option">
+            <input
+              type="radio"
+              id="monthly"
+              name="time"
+              value="monthly"
+              checked={selectedTime === 'monthly'}
+              onChange={handleTimeChange}
+            />
+            <label htmlFor="monthly">Monthly</label>
+          </div>
+          <div className="time-option">
+            <input
+              type="radio"
+              id="oneOff"
+              name="time"
+              value="oneOff"
+              checked={selectedTime === 'oneOff'}
+              onChange={handleTimeChange}
+            />
+            <label htmlFor="oneOff">One-off</label>
+          </div>
+        </div>
 
         <div className="amount-selector">
           <select value={selectedAmount} onChange={handleAmountChange}>
-          <option value="$10">£5</option>
+            <option value="$5">£5</option>
             <option value="$10">£10</option>
             <option value="$20">£20</option>
             <option value="$50">£50</option>
             <option value="$100">£100</option>
-            <option value="$100">£100,000,000</option>
+            <option value="$100000000">£100,000,000</option>
           </select>
         </div>
         <button className="donate-button" onClick={handleSubmit}>
@@ -87,6 +90,7 @@ const DonationsPage = () => {
         </button>
       </div>
       <div className="donation-image"></div>
+      {/* Uncomment the following section if you want to include the donation amount input and donate now button */}
       {/* <div>
         <p>Donation amount: £{donationAmount}</p>
         <p>Total: £{totalAmount}</p>
