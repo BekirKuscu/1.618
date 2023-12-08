c11af749f7eb029262d8909043b2d9917fd2a380
onurat <114289826+onurat@users.noreply.github.com>
2023-12-08 16:16:48 +0000

    New changes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Header from '../../Components/Header-Content/Header';
 import WasteDetail from '../../Components/WasteDetail-Content/WasteDetail';
-// import WasteQuote from '../../Components/WasteQuotes-Conten/WasteQuote';
+import Welcome from '../../Components/Welcome-Content/Welcome';
 import TeamSection from '../../Components/Our-Team/TeamSection';
+import OurMission from '../../Components/OurMission-Content/OurMission';
 
 function Home() {
-return (
+  return (
     <div>
+      <Welcome />
       <Header />
+      <OurMission />
       <WasteDetail />
       <TeamSection />
-      {/* <WasteQuote /> */}
     </div>
-)
-  }
+  )
+}
 
 export default Home;
